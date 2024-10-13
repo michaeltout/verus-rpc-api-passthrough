a5400d8e4579803ccaca034f29afa19b721efa16
michaeltout <toutonghi.michael@gmail.com>
2024-10-13 16:48:55 +0200

    Add prices file

--- env.js
+++ env.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.API_PARAMETER_ARRAY = exports.API_METHOD = exports.SYSTEM_ID = exports.RPC_PORT = exports.RPC_PASS = exports.RPC_USER = void 0;
+const prices_2023_1 = require("./prices_2023");
 exports.RPC_USER = "";
 exports.RPC_PASS = "";
 exports.RPC_PORT = 27486;
 exports.SYSTEM_ID = "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV";
-exports.API_METHOD = "getidentity";
-exports.API_PARAMETER_ARRAY = ["test@"];
+exports.API_METHOD = "listtransactions";
+exports.API_PARAMETER_ARRAY = [{
+        nativeprices: prices_2023_1.prices,
+        costbasisdata: {},
+        offchaintransfers: [],
+        fromblock: 2350305,
+        toblock: 2859403
+    }, 1000000];
--- env.ts
+++ env.ts
@@ -1,7 +1,15 @@
+import { prices } from "./prices_2023";
+
 export const RPC_USER = "";
 export const RPC_PASS = "";
 export const RPC_PORT = 27486;
 export const SYSTEM_ID = "i5w5MuNik5NtLcYmNzcvaoixooEebB6MGV";
 
-export const API_METHOD = "getidentity"
-export const API_PARAMETER_ARRAY = ["test@"]
+export const API_METHOD = "listtransactions"
+export const API_PARAMETER_ARRAY = [{
+  nativeprices: prices,
+  costbasisdata: {},
+  offchaintransfers: [],
+  fromblock: 2350305,
+  toblock: 2859403
+}, 1000000]
